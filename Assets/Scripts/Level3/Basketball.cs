c007586c83cfa26419d18c6848349cdb172c6914
Maisam Mohammadi <riomaisam@gmail.com>
2026-06-22 18:03:14 +0200

    success Particles is added to the basketball

--- Assets/Scripts/Level3/Basketball.cs
+++ Assets/Scripts/Level3/Basketball.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class Basketball : MonoBehaviour
 {
     [SerializeField] GameObject movingPlatform;
+    [SerializeField] private ParticleSystem successParticles;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<FireHead>() != null)
         {
             movingPlatform.GetComponent<MovingPlatform>().SetShouldMoving(true);
+            successParticles.Play();
         }
     }
 }
